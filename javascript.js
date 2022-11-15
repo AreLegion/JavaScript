ca9f3b5b927388a3e56c8c2c25e2d61c39fa0a62
Doston <arelegion@gmail.com>
2022-11-15 22:43:22 +0300

    JavaScript

---
+++ javascript.js
@@ -0,0 +1,23 @@
+var modal = prompt ( "Ismingizni Kiriting...")
+console.log("Sizning Ismingiz: "+modal);
+var age = +prompt ("Yoshingizni Kiriting....")
+console.log("Sizning Yoshingiz: "+age);
+
+var misol = +prompt ("7+8=?")
+console.log(" 1️⃣) 7+8=15 ✔️ (Sizning Javobingiz:) " +misol);
+
+var misol = +prompt ("15-7=?")
+console.log(" 2️⃣) 15-7=8 ✔️ (Sizning Javobingiz:) " +misol);
+
+var misol = +prompt ("7*70=?")
+console.log(" 3️⃣) 7*70=490 ✔️ (Sizning Javobingiz:) " +misol);
+
+var misol = +prompt ("7/20=?")
+console.log(" 4️⃣) 7/20=0.35 ✔️ (Sizning Javobingiz:) " +misol);
+
+var misol = +prompt ("70%10=?")
+console.log(" 5️⃣) 70%10=2 ✔️ (Sizning Javobingiz:) " +misol);
+
+alert ("Kirish  Uchun  Bosing...                                                                   👇🏻")
+
+
